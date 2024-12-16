9a59e239b158fecc97ed0cbc8f8111db2bbdee30
samue1eun <samuel.eun.park@gmail.com>
2024-12-15 22:29:46 -0700

    more backend work

--- backend/crypto/urls.py
+++ backend/crypto/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
-from django.shortcuts import render
-from . import views
-from .views import CryptoCurrencyListView, CryptoCurrencyDetailView, UserFavoriteCryptoListView, UserFavoritesCryptoCreateView, RemoveFavoriteCryptoView
-
-# Create your views here.
+from .views import (
+    CryptoCurrencyListView, CryptoCurrencyDetailView, UserFavoriteCryptoListView, 
+    UserFavoritesCryptoCreateView, RemoveFavoriteCryptoView
+)
 
 urlpatterns = [
-    path('cryptos/', views.CryptoCurrencyListView.as_view(), name='crypto-list'),
-    path('cryptos/<int:pk>/', views.CryptoCurrencyDetailView.as_view(), name='crypto-detail'),
-    path('crypto/user_favorites/', views.UserFavoriteCryptoListView.as_view(), name='user-favorites'),
-    path('crypto/user_favorites/add/', views.UserFavoritesCryptoCreateView.as_view(), name='user-add-favorite'),
-    path('crypto/user_favorites/remove/', views.RemoveFavoriteCryptoView.as_view(), name='user-remove-favorite'),
+    path('cryptos/', CryptoCurrencyListView.as_view(), name='crypto-list'),
+    path('cryptos/<int:pk>/', CryptoCurrencyDetailView.as_view(), name='crypto-detail'),
+    path('crypto/user_favorites/', UserFavoriteCryptoListView.as_view(), name='user-favorites'),
+    path('crypto/user_favorites/add/', UserFavoritesCryptoCreateView.as_view(), name='user-add-favorite'),
+    path('crypto/user_favorites/remove/', RemoveFavoriteCryptoView.as_view(), name='user-remove-favorite'),
 ]
