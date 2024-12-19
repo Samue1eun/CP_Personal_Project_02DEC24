663605e63af3d490d3874efa5b6d2429db55bf50
samue1eun <samuel.eun.park@gmail.com>
2024-12-19 15:14:20 -0700

    register form

--- frontend/components/Code Pen/Login_Register_2/RegisterForm.jsx
+++ frontend/components/Code Pen/Login_Register_2/RegisterForm.jsx
@@ -28,49 +28,52 @@ const Register = () => {
 
     return (
         <>
-        <div className="register-container">
-        <form className="register-form" onSubmit={handleSubmit}>
-            <h3 className="register-title">Register</h3>
-            <label className="username-title" for="username">Username</label>
-            <input
-                className="register-username-input"
-                type="text" 
-                id="username" 
-                placeholder="Type in your username.." 
-                autocomplete="off" 
-                required
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <label className="email-title" for="email">Email</label>
-            <input
-                className="register-email-input"
-                type="email" 
-                id="email" 
-                name="email" 
-                placeholder="Enter your Email.." 
-                autocomplete="off" 
-                required 
-                value = {email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <label className="password-title" for="password">Password</label>
-            <input
-                className="register-password-input"
-                type="password" 
-                id="password" 
-                name="password" 
-                placeholder="Enter your password.." 
-                autocomplete="off" 
-                required 
-                value = {password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <div className="register-log-in-button-container">
-                <button class="go-back-to-login-button button-clear" onClick={() => navigate('/login')}>Register</button>
-                <button type="submit" class="register-button button-outline">Log In</button>
+        <div className="register-page-container">
+            <div className="register-container">
+            <h1>Register to be in Crypto Peace</h1>
+            <form className="register-form" onSubmit={handleSubmit}>
+                <h3 className="register-title">Register</h3>
+                <label className="username-title" for="username">Username</label>
+                <input
+                    className="register-username-input"
+                    type="text" 
+                    id="username" 
+                    placeholder="Type in your username.." 
+                    autocomplete="off" 
+                    required
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <label className="email-title" for="email">Email</label>
+                <input
+                    className="register-email-input"
+                    type="email" 
+                    id="email" 
+                    name="email" 
+                    placeholder="Enter your Email.." 
+                    autocomplete="off" 
+                    required 
+                    value = {email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <label className="password-title" for="password">Password</label>
+                <input
+                    className="register-password-input"
+                    type="password" 
+                    id="password" 
+                    name="password" 
+                    placeholder="Enter your password.." 
+                    autocomplete="off" 
+                    required 
+                    value = {password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <div className="register-log-in-button-container">
+                    <button class="go-back-to-login-button button-clear" onClick={() => navigate('/login')}>Go Back to Log In</button>
+                    <button type="submit" class="register-button button-outline">Register</button>
+                </div>
+            </form>
             </div>
-        </form>
         </div>
         </>
     )
