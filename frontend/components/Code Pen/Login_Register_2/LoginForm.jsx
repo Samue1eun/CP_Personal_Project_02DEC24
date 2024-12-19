17abfb85c5d4a00a1d460457e3f9e4de5b7ed78d
samue1eun <samuel.eun.park@gmail.com>
2024-12-19 15:13:27 -0700

    added in components

--- frontend/components/Code Pen/Login_Register_2/LoginForm.jsx
+++ frontend/components/Code Pen/Login_Register_2/LoginForm.jsx
@@ -27,35 +27,41 @@ const LogIn = () => {
 
     return (
         <>
-        <div className="log-in-container">
-        <form className="log-in-form" onSubmit={handleSubmit}>
-            <h3 className ="log-in-title">Log In</h3>
-            <label className ="username-title" for="username">Username</label>
-            <input 
-                type="text" 
-                class="log-in-username-input" 
-                placeholder="Type in your username.." 
-                autocomplete="off" 
-                required
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <label className="password-title" for="password">Password</label>
-            <input 
-                type="password" 
-                class="log-in-password-input" 
-                name="password" 
-                placeholder="Enter your password.." 
-                autocomplete="off" 
-                required 
-                value = {password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <div className="log-in-button-container">
-                <button class="log-in-register-button button-clear" onClick={() => navigate('/register')}>Register</button>
-                <button type="submit" class="log-in-button button-outline">Log In</button>
+        <div className="log-in-page-container">
+        <video autoPlay muted loop className="background-video">
+            <source src="./media/website_background_video.mov" type="video/mp4" />
+        </video>
+            <div className="log-in-container">
+                <h1 className="log-in-header">Welcome to your Crypto Tracker, Yogi</h1>
+            <form className="log-in-form" onSubmit={handleSubmit}>
+                <h3 className ="log-in-title">Log In</h3>
+                <label className ="username-title" for="username">Username</label>
+                <input 
+                    type="text" 
+                    class="log-in-username-input" 
+                    placeholder="Type in your username.." 
+                    autocomplete="off" 
+                    required
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <label className="password-title" for="password">Password</label>
+                <input 
+                    type="password" 
+                    class="log-in-password-input" 
+                    name="password" 
+                    placeholder="Enter your password.." 
+                    autocomplete="off" 
+                    required 
+                    value = {password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <div className="log-in-button-container">
+                    <button class="log-in-register-button button-clear" onClick={() => navigate('/register')}>Register</button>
+                    <button type="submit" class="log-in-button button-outline">Log In</button>
+                </div>
+            </form>
             </div>
-        </form>
         </div>
         </>
     )
